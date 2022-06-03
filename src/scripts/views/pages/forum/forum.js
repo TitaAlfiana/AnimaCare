b3384ee952ed994db25a61a4a3398e5dca2d96f8
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-06-03 15:44:46 +0700

    update-hendra

--- src/scripts/views/pages/forum/forum.js
+++ src/scripts/views/pages/forum/forum.js
@@ -8,10 +8,38 @@ const Forum = {
     nav.style.display = 'flex';
     const footer = document.querySelector('.footer');
     footer.style.display = 'block';
-    
+
     return `
-    <div class="container>
-      <h1>Forum</h1>
+    <div class="container">
+    <!-- As a heading -->
+    <a class="navbar-brand" href="#">+ Bagi Pengalaman</a>
+    <form class="form-inline">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+
+    <div class="card" w-100>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Button</a>
+      </div>
+    </div>
+
+    <div class="card" w-100>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Button</a>
+      </div>
+    </div>
+
+    <div class="card" w-100>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Button</a>
+      </div>
     </div>
     `;
   },
