94cf7773dcd278bff5f5933b832254c5c4e5828b
Ibnu Choldun <96803344+ibnucholdun@users.noreply.github.com>
2023-03-23 22:11:03 +0700

    update API and Database

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  BASE_URL: 'https://animacare.herokuapp.com/v1',
-  BASE_IMAGE_URL: 'https://animacare.herokuapp.com/images/',
+  BASE_URL: 'https://animacare-api.cyclic.app/v1',
+  BASE_IMAGE_URL: 'https://animacare-api.cyclic.app/images/',
   CACHE_NAME: 'AnimaCare',
   DATABASE_NAME: 'AnimaCare-database',
   DATABASE_VERSION: 1,
